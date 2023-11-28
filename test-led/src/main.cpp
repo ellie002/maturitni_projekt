30dab7fb11a6d6351db4c88696589f62cec9ffa8
student <student@sspu-opava.cz>
2023-11-28 14:54:04 +0100

    blik blik

--- test-led/src/main.cpp
+++ test-led/src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int buttonPin = 2;    //  button is connected to pin
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+bool buttonState = 0;       // variable to hold the button state
+bool Mode = 0;              // What mode is the light in?
+
+
+void setup()
+{
+  pinMode(buttonPin, INPUT_PULLUP);    // Set the switch pin as input
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  if (!digitalRead(buttonPin)) // cheks if button is pressed (cheks if it's LOW, becouse PULLUP inverts signal: LOW = button pressed)
+  {
+    if (!buttonState) // same as: (buttonState == false) ---I made it a bit compact, but it may be confusinf to you
+    {
+      buttonState = true;
+      Mode = !Mode; // this inverts button mode: If Mode was True - it will make it False and viseversa
+    }
+  }
+  else buttonState = false;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // blink LED
+  digitalWrite(LED_BUILTIN, Mode); // remember: TRUE = HIGH = 1 and FALSE = LOW = 0 (You can use whatever You like)
+  delay(5);
 }
